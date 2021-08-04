aaf069a2563433d43f8aae0a862732bccdc3eba2
hansenconnor <connorhnsn5596@gmail.com>
2021-08-04 16:01:06 -0400

    Added subhero parallax section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Header from "../components/Header";
 import { useSpring, animated, config } from "react-spring";
 import Layout from "../components/Layout";
+import gallery from "../public/gallery.png"
 
 function Sphere() {
   const styles = useSpring({
@@ -35,14 +36,42 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Neon Mode Designer." />
+        <title>Connor Hansen</title>
+        <meta name="description" content="Neon Mode Designer ✣ Creative Director ✣ Fashion Photographer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* <Parallax style={{background: "black"}} pages={3} className="h-full flex flex-col">
+        <ParallaxLayer offset={0} style={{background: "red"}}>
 
-      <Parallax pages={2} className="h-full flex flex-col">
-        <ParallaxLayer offset={0} speed={-0.4} className="flex items-center z-10">
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} style={{background: "green"}}>
+
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} style={{background: "blue"}}>
+
+        </ParallaxLayer>
+      </Parallax> */}
+
+
+      <Parallax pages={3} config={config.molasses} className="h-full flex flex-col">
+
+        
+
+        {/* First Section */}
+
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          style={{ backgroundColor: "#FFF" }}>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0.6} speed={-0.4} className="z-0">
+          <div className="absolute left-1/3">
+            <Sphere></Sphere>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.4} speed={-0.6}>
           <div className="container mx-auto">
             <h3 className="text-2xl w-11/12 sm:w-11/12 sm:text-3xl sm:leading-relaxed leading-normal md:text-4xl md:w-11/12 lg:w-5/6 md:leading-normal text-gray-600">
               Combining{" "}
@@ -57,39 +86,28 @@ export default function Home() {
           </div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={0.6} speed={0.8}>
-          <div className="absolute left-1/3">
-            <Sphere></Sphere>
-          </div>
-        </ParallaxLayer>
 
+
+        {/* Second Section */}
         <ParallaxLayer
           offset={1}
           speed={0}
-          style={{ backgroundColor: "#F0F0F0" }}
-          className="z-10"
-        />
-
-        {/* <ParallaxLayer
-          offset={1}
-          speed={2.5}
-          style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <p>Keep Scrolling</p>
+          style={{ backgroundColor: "#FAFAFA" }}>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={2} speed={2} style={{ backgroundColor: '#101010' }} />
+        <ParallaxLayer offset={1.1} speed={0} style={{ backgroundColor: "#F0F0F0", width: "80%", height: "80vh", margin: "0 auto", overflow: "hidden" }}>
+          <ParallaxLayer factor={1.2} offset={0} speed={0.25} style={{ backgroundColor: "#F0F0F0" }}>
+            <Image src={gallery} layout={"fill"} objectFit={"cover"}></Image>
+          </ParallaxLayer>
+        </ParallaxLayer>
 
+        {/* Third Section */}
         <ParallaxLayer
           offset={2}
-          speed={0.5}
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: 'white',
-          }}>
-          <p>Scroll up</p>
-        </ParallaxLayer> */}
+          speed={0}
+          style={{ backgroundColor: "#FFF" }}>
+        </ParallaxLayer>
+        
       </Parallax>
 
       <footer></footer>
